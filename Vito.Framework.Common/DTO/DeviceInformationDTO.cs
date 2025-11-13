7949af5d39335618ae022db0d5876c983593b0f9
Ever Alonso Torres Galeano <eeatg844@hotmail.com>
2025-11-13 08:56:23 -0500

    Mark field as non nullable for device info

--- Vito.Framework.Common/DTO/DeviceInformationDTO.cs
+++ Vito.Framework.Common/DTO/DeviceInformationDTO.cs
@@ -2,23 +2,23 @@
 
 public record DeviceInformationDTO
 {
-    public string? Id;
-    public string? HostName;
-    public string? IpAddress;
-    public string? DeviceType;
-    public string? Browser;
-    public string? Platform;
-    public string? Engine;
-    public string? CultureId;
-    public string? EndPointPattern;
-    public string? EndPointUrl;
-    public string? Method;
-    public string? QueryString;
-    public string? UserAgent;
-    public string? Referer;
-    public long? ApplicationId;
-    public long? CompanyId;
-    public long? RoleId;
-    public long? UserId;
+    public string Id = string.Empty;
+    public string HostName = string.Empty;
+    public string IpAddress = string.Empty;
+    public string DeviceType = string.Empty;
+    public string Browser = string.Empty;
+    public string Platform = string.Empty;
+    public string Engine = string.Empty;
+    public string CultureId = string.Empty;
+    public string EndPointPattern = string.Empty;
+    public string EndPointUrl = string.Empty;
+    public string Method = string.Empty;
+    public string QueryString = string.Empty;
+    public string UserAgent = string.Empty;
+    public string Referer= string.Empty;
+    public long ApplicationId;
+    public long CompanyId;
+    public long RoleId;
+    public long UserId;
 };
 
